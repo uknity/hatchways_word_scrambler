0df5da64901be1e912ea16d99e76642c5675cafa
Liz Emerson <uknity@gmail.com>
2021-06-16 06:13:26 -0400

    working on scrambling function

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,27 +3,59 @@ import { useState, useEffect } from "react";
 import API from "../utils/API";
 
 const Homepage = () => {
-	const [counter, setCounter] = useState();
-    const [displaySentence, setDisplaySentence] = useState("");
+	const [displaySentence, setDisplaySentence] = useState("");
+	const [scrambledSentence, setScrambledSentence] = useState("");
+
+	const counter = 1;
 
 	useEffect(() => {
 		loadGame();
 	});
 
-    
-	const loadGame = () => {
-        const sentenceNum = 1;
-		console.log("you are in loadgame function");
-		setCounter(sentenceNum);
-        console.log(counter);
-        API.getSentence(counter)
-        .then(res => {
-            setDisplaySentence(res.data.data.sentence);
-            // const displaySentence = res.data.data.sentence;
-            // console.log(displaySentence);
-        })
-        .catch(err => console.log(err));
+	const scrambleWord = (word) => {
+		console.log(word);
+		var length = word.length;
+
+		var letterArr = word.split("");
+		console.log(letterArr);
+		letterArr.map((letter) => {
+
+			for (var i = 1; i <= length - 1; i++) {
+				var j = Math.floor(Math.random() * length);
+				var temp = letterArr[i];
+				letterArr[i] = letterArr[j];
+				letterArr[j] = temp;
+			};
+			
+		});
+		var scrambledWord = letterArr.join('');
+			console.log(scrambledWord);
+			return scrambledWord;
 	};
+
+	const scrambleSentence = (sentence) => {
+		const sentenceArr = sentence.split(" ");
+		sentenceArr.map((word) => {
+			if (word.length <= 2) {
+			} else {
+				var scrambledWord = scrambleWord(word);
+				console.log(scrambledWord);
+			};
+			var newSentence = sentenceArr.join(' ');
+			console.log(newSentence);
+		});
+		// 
+	};
+
+	const loadGame = () => {
+		API.getSentence(counter)
+			.then((res) => {
+				setDisplaySentence(res.data.data.sentence);
+				scrambleSentence(displaySentence);
+			})
+			.catch((err) => console.log(err));
+	};
+
 	return (
 		<div className="App">
 			<div className="page-container container-fluid">
