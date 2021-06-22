441d3023cbe42f38329aeb0ef0ff7525bd075983
Liz Emerson <uknity@gmail.com>
2021-06-22 18:37:52 -0400

    object created

--- src/components/WordRow/index.js
+++ src/components/WordRow/index.js
@@ -33,28 +33,32 @@ const array = [
 		}
 	];
 
-	const counter = 0;
+	var counter = 0;
 
 	const addIds = () => {
 		array.map((word) => {
 			for (let i=0; i < word.length; i++) {
-				word.letters[i].id = counter.toString;
-			}
+				word.letters[i].letter.id = counter;
+			}	
 			
-		
-		
 		})
 	}
 	
-
 	const createLetterArray = () => {
 		for (let i=0; i < array.length; i++) {
 			const wordToSplit = array[i].word;
 			console.log(wordToSplit);
 			const letterAr = wordToSplit.split("");
-			console.log(letterAr);
-			array[i].letters = ({letters: letterAr})
 			
+			let indexedLetters = [];
+
+			for (let letterIndex=0; letterIndex < letterAr.length; letterIndex++) {
+				indexedLetters.push([counter, letterAr[letterIndex]]);
+				counter++;
+			}
+			console.log(letterAr);
+			console.log(indexedLetters);
+			array[i].letters = ({indexedLetters})
 		}
 		addIds();
 	}
@@ -77,105 +81,10 @@ const array = [
 
 	createWordArray();
 	console.log(array);
+	console.log(array[0].word);
+	// console.log(array[0].letters);
 
-	// const wordsAndLettersObject = 
-	// 	sentArray.forEach((word) => {
-	// 	object.push({
-	// 		'word': (word) + " ",
-	// 		'letters': (word.split(""))
-	// 	})
-		
-	// });
-	// console.log(object);
 	
-	const wordObject = [
-		{
-			word: "chocolate",
-			letters: [
-				{
-					letter: "c",
-					id: 0
-				},
-				{
-					letter: "h",
-					id: 1
-				},
-				{
-					letter: "o",
-					id: 2
-				},
-				{
-					letter: "c",
-					id: 3
-				},
-				{
-					letter: "o",
-					id: 4
-				},
-				{
-					letter: "l",
-					id: 5
-				},
-				{
-					letter: "a",
-					id: 6
-				},
-				{
-					letter: "t",
-					id: 7
-				},
-				{
-					letter: "e",
-					id: 8
-				},
-				{
-					letter: " ",
-					id: 9
-				},
-				]
-		},
-		{
-			word: "is",
-			letters: [
-				{
-					letter: "i",
-					id: 10
-				},
-				{
-					letter: "s",
-					id: 11
-				},
-				{
-					letter: " ",
-					id: 12
-				}
-			]
-
-		},
-		{
-			word: "good",
-			letters: [
-				{
-					letter: "g",
-					id: 13
-				},
-				{
-					letter: "o",
-					id: 14
-				},
-				{
-					letter: "o",
-					id: 15
-				},
-				{
-					letter: "d",
-					id: 16
-				},
-			]
-			
-		}
-	]
-	console.log(wordObject);
 	
 	window.addEventListener("keydown", (event) => {
 		setInputValue(event.key);
