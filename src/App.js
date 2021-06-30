90bf950cbfe8eaf041e3ae6b832b2e85dc6f0a56
Liz Emerson <uknity@gmail.com>
2021-06-30 12:03:57 -0400

    corrected looping bug - working on validatingId

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import Homepage from "./pages/Homepage";
 import Outcome from "./pages/Outcome";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
-import Wrapper from "./components/Wrapper";
-
 
 function App() {
 	return (
 		<Router>
 			<div>
-				<Wrapper>
-					<Switch>
-						<Route exact path="/" component={Homepage} />
-						<Route exact path="/outcome" component={Outcome} />
-						<Route component={NoMatch} />
-					</Switch>
-					</Wrapper>
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route exact path="/outcome" component={Outcome} />
+					<Route component={NoMatch} />
+				</Switch>
 			</div>
 		</Router>
 	);
--- src/components/WordRow/index.js
+++ src/components/WordRow/index.js
@@ -4,130 +4,95 @@ import { useEffect, useState, useCallback } from "react";
 
 function WordRow(props) {
 	const { sent, score, sentenceCompleted, calcScore } = props;
-	
-	// console.log('sent', sent);
-	// if (sent.length === 0) {
-	// 	return null;
-	// };
-	//counter used to increment for unique letter keys
-	//validating id is used to pull the ids from the letters
-	// const [counter, setCounter] = useState(0);
-	// const [validatingId, setValidatingId] = useState(0);
-	
-
-	// useEffect(() => {
-	// 	console.log('wordrow mounted');
-	// 	resetCounter();
-	// 	resetValidatingId();
-	// }, []);
-
 	//retrieves the array of sentence words
 	const sentArray = Object.values(sent);
-	console.log('sentArray', sentArray);
-	
-	let counter = 0;
-	let validatingId = 0;
+	console.log("sentArray", sentArray);
+	const letterArray = sentArray.join(" ").split("");
+	console.log('letterArray length', letterArray.length);
+	const sentenceFinished = letterArray.length;
+	console.log("sentence finished", sentenceFinished);
+
+	//counter used to increment for unique letter keys
+	//validating id is used to pull the ids from the letters
+	var counter = 0;
+	var validatingId = 1;
 
 	//increments the counter for the key of each letter in the sentence
 	const counterFunc = () => {
-		console.log('in counterFunc');
 		counter++;
+		return counter;
 	};
 
+	const validIdFunc = () => {
+		validatingId++;
+		return validatingId;
+	};
+
+	//reset counter upon new sentence load
 	const resetCounter = () => {
-		counter = 0;
+		return counter = 0;
 	};
 
+	//reset validating id upon new sentence load
 	const resetValidatingId = () => {
-		validatingId = 0;
+		return validatingId = 1;
 	};
 
-	const letterArray = sentArray.join(" ").split("");
-
-	var sentenceFinished = letterArray.length - 1;
-
-	console.log('sentence finished', sentenceFinished);
-
-	
-
-	const handleKeyDown = (event) => {
-		const { key } = event;
-		console.log('counter', counter);
+	const handleKeyDown = ({ key }) => {
+		console.log("in handle key down func", validatingId);
+		console.log("key", key);
 		var letterCol = document.getElementById(`${validatingId}`);
-	console.log(document.getElementById(0));
-		console.log(letterCol);
 		var letterSpan = letterCol.firstElementChild;
-		console.log(letterSpan);
-	  };
+		if (letterSpan === null) {
+			if (key === " ") {
+				letterCol.classList.remove("spaceNotGuessed");
+				letterCol.classList.add("spaceGuessed");
+				validIdFunc();
+				// validatingId++;
+			} else {
+				console.log("please press the space key");
+			}
+		} else {
+			var letterId = letterSpan.textContent.toLowerCase();
+			console.log('letter id', letterId);
+
+			if (letterId === key) {
+				letterCol.classList.remove("notGuessedSpace");
+				letterCol.classList.add("guessedSpace");
+				letterSpan.classList.remove("notGuessed");
+				letterSpan.classList.add("guessed");
+				console.log("validating id", validatingId);
+				console.log('sentenceFinished', sentenceFinished);
+
+				if (validatingId === sentenceFinished) {
+					console.log("good job");
+					resetCounter();
+					resetValidatingId();
+					props.sentenceCompleted();
+				} else {
+					validIdFunc();
+					// validatingId++;
+				};
+			};
+			} 
+		};
+
+		const handleKeyUp = () => {
+
+		}
 	
-	  useEffect(() => {
-		  resetValidatingId();
-		  resetCounter();
-		window.addEventListener('keydown', handleKeyDown);
-		// window.addEventListener('keyup', handleKeyUp);
-	// 	window.removeEventListener('keydown', handleKeyDown)
-    //   window.removeEventListener('keyup', handleKeyUp)
+
+	useEffect(() => {
+		window.addEventListener("keydown", handleKeyDown);
+		window.addEventListener("keyup", handleKeyUp);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+			window.removeEventListener("keyup", handleKeyUp);
+		};
 	}, []);
-	
-		
-		// window.addEventListener("keydown", (event) => {
-		// event.preventDefault();
-		// var onKeyPressed = (event) => {
-		// 	console.log(event.key);
-		// }
-	// 	console.log('validatingId', validatingId);
-	// 	console.log('counter', counter);
-	// 	var letterCol = document.getElementById(`${validatingId}`);
-	// 	var letterSpan = letterCol.firstElementChild;
-
-	// 	if (letterSpan === null) {
-	// 		if (event.keyCode === 32) {
-	// 			console.log('in event letterSpan null and keyCode 32');
-	// 			letterCol.classList.remove("spaceNotGuessed");
-	// 			letterCol.classList.add("spaceGuessed");
-	// 			validatingId++;
-	// 			window.removeEventListener("keydown", event)
-	// 		} else {
-	// 			console.log("please press the space key");
-	// 			window.removeEventListener("keydown", event)
-	// 		}
-	// 	} 
-		
-	// 	else {
-	// 		var letterId = letterSpan.textContent.toLowerCase();
-
-	// 		if (letterId === event.key) {
-	// 			letterCol.classList.remove("notGuessedSpace");
-	// 			letterCol.classList.add("guessedSpace");
-	// 			letterSpan.classList.remove("notGuessed");
-	// 			letterSpan.classList.add("guessed");
-	// 			console.log('validating id', validatingId);
-	// 			console.log(sentenceFinished);
-
-	// 			if (validatingId === sentenceFinished) {
-	// 				event.preventDefault();
-	// 				console.log("good job");
-	// 				window.removeEventListener("keydown", event)
-	// 				console.log(validatingId);
-	// 				props.sentenceCompleted();
-	// 			} else {
-	// 				validatingId++;
-	// 				window.removeEventListener("keydown", event)
-	// 			};
-				
-	// 		} else {
-	// 			console.log('please try again');
-	// 		}
-	// 	}
-	// })
-
-	// }, []);
 
 	return (
-		
 		<div>
-			
-			
 			{sentArray.map((word, index) => (
 				<div className="row" key={word + index}>
 					{word.split("").map((letter) => (
@@ -136,8 +101,6 @@ function WordRow(props) {
 							key={letter.id}
 							className={`col letterSpace notGuessedSpace`}
 							onKeyDown={handleKeyDown}
-							// onKeyUp={handleKeyUp}
-							// tabIndex={0}
 						>
 							<span className={"notGuessed"}>{letter}</span>
 						</div>
@@ -147,9 +110,8 @@ function WordRow(props) {
 						<div
 							id={counterFunc()}
 							onKeyDown={handleKeyDown}
-							key={'space' + index}
+							key={"space" + index}
 							className={`col space spaceNotGuessed`}
-							// tabIndex={0}
 						>
 							{" "}
 						</div>
@@ -158,12 +120,61 @@ function WordRow(props) {
 					)}
 				</div>
 			))}
-				
 		</div>
-					);
+	);
 }
 
 export default WordRow;
+
+//const handleKeyUp = ({ key }) => {
+	// 	var letterCol = document.getElementById(`${validatingId}`);
+	// 	var letterSpan = letterCol.firstElementChild;
+	// 	if (letterSpan === null) {
+	// 		if (key === " ") {
+	// 			// setKeyPressed(false);
+	// 			letterCol.classList.remove("spaceNotGuessed");
+	// 			letterCol.classList.add("spaceGuessed");
+	// 			validatingId++;
+	// 		} else {
+	// 			console.log("please press the space key");
+	// 		}
+	// 	} else {
+	// 		var letterId = letterSpan.textContent.toLowerCase();
+	// 		console.log(letterId);
+
+	// 		if (letterId === key) {
+	// 			letterCol.classList.remove("notGuessedSpace");
+	// 			letterCol.classList.add("guessedSpace");
+	// 			letterSpan.classList.remove("notGuessed");
+	// 			letterSpan.classList.add("guessed");
+	// 			console.log("validating id", validatingId);
+	// 			console.log(sentenceFinished);
+
+	// 			if (validatingId === sentenceFinished) {
+	// 				console.log("good job");
+	// 				console.log(validatingId);
+	// 				props.sentenceCompleted();
+	// 			} else {
+	// 				validatingId++;
+	// 			};
+	// 		};
+	// 		} 
+	// 	} 
+	// };
+
+// console.log('sent', sent);
+// if (sent.length === 0) {
+// 	return null;
+// };
+
+//const [counter, setCounter] = useState(0);
+// const [validatingId, setValidatingId] = useState(0);
+
+// useEffect(() => {
+// 	console.log('wordrow mounted');
+// 	resetCounter();
+// 	resetValidatingId();
+// }, []);
 
 // switch(letterSpan) {
 // 	case (letterSpan === null && event.keyCode === 32):
