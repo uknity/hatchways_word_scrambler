019159a8cc0045e6fc204674c7801e21c33a5363
Liz Emerson <uknity@gmail.com>
2021-07-05 20:42:51 -0400

    corrected sentence finished and score function

--- src/components/WordRow/index.js
+++ src/components/WordRow/index.js
@@ -1,34 +1,23 @@
 import React from "react";
 import "./style.css";
 import { useEffect, useState, useCallback } from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../../features/counter/counterSlice';
 
 function WordRow(props) {
 	const { sent, score, sentenceCompleted, calcScore } = props;
+
+	var wordRowAdded = false;
+
 	//retrieves the array of sentence words
 	const sentArray = Object.values(sent);
-	console.log("sentArray", sentArray);
+
+	//console.log("sentArray", sentArray);
 	const letterArray = sentArray.join(" ").split("");
-	console.log('letterArray length', letterArray.length);
-	
-	window.sentenceFinished  = letterArray.length;
-	
-	console.log("sentence finished", window.sentenceFinished);
 
-	// const [validatingIndex, setValidatingIndex] = useState(1);
+	window.sentenceFinished = letterArray.length;
 
-	// const incrementValidatingIndex = () => {
-	// 	setValidatingIndex(prevValidatingIndex => prevValidatingIndex + 1);
-	// }
-
-	//counter used to increment for unique letter keys
-	//validating id is used to pull the ids from the letters
 	var counter = 0;
 
-	// const validatingIndex = useSelector(state => state.counter.value);
-	// const dispatch = useDispatch();
-	
+	window.validatingId = 1;
 
 	//increments the counter for the key of each letter in the sentence
 	const counterFunc = () => {
@@ -36,70 +25,54 @@ function WordRow(props) {
 		return counter;
 	};
 
-	window.validatingIndex = 1;
-
-	// const validIdFunc = () => {
-	// 	validatingId++;
-	// 	return validatingId;
-	// };
+	const validIdFunc = () => {
+		window.validatingId++;
+		return window.validatingId;
+	};
 
 	//reset counter upon new sentence load
 	const resetCounter = () => {
-		return counter = 0;
+		return (counter = 0);
 	};
 
 	//reset validating id upon new sentence load
-	// const resetValidatingId = () => {
-	// 	return validatingId = 1;
-	// };
+	const resetValidatingId = () => {
+		return (window.validatingId = 1);
+	};
 
 	const handleKeyDown = ({ key }) => {
-		console.log("in handle key down func", window.validatingIndex);
-		console.log("key", key);
-		var letterCol = document.getElementById(`${window.validatingIndex}`);
+		var letterCol = document.getElementById(`${window.validatingId}`);
 		var letterSpan = letterCol.firstElementChild;
-		console.log(letterSpan);
-		if (letterSpan === null) {
+
+		if (letterSpan === null || letterSpan === undefined) {
 			if (key === " ") {
 				letterCol.classList.remove("spaceNotGuessed");
 				letterCol.classList.add("spaceGuessed");
-				// dispatch(increment());
-				// validatingId++;
-				window.validatingIndex++;
+				validIdFunc();
 			} else {
 				console.log("please press the space key");
 			}
 		} else {
 			var letterId = letterSpan.textContent.toLowerCase();
-			console.log('letter id', letterId);
 
 			if (letterId === key) {
 				letterCol.classList.remove("notGuessedSpace");
 				letterCol.classList.add("guessedSpace");
 				letterSpan.classList.remove("notGuessed");
 				letterSpan.classList.add("guessed");
-				console.log("validating id", window.validatingIndex);
-				console.log('sentenceFinished', window.sentenceFinished);
-
-				if (window.validatingIndex === window.sentenceFinished) {
-					console.log("good job");
+if (window.validatingId === window.sentenceFinished) {
 					resetCounter();
-					// resetValidatingId();
+					resetValidatingId();
+					validIdFunc();
 					props.sentenceCompleted();
 				} else {
-					// incrementwindow.validatingIndex();
-					window.validatingIndex++;
-					console.log(window.validatingIndex);
-					// validatingId++;
-				};
-			};
-			} 
-		};
-
-		const handleKeyUp = () => {
-
+					window.validatingId++;
+				}
+			}
 		}
-	
+	};
+
+	const handleKeyUp = () => {};
 
 	useEffect(() => {
 		window.addEventListener("keydown", handleKeyDown);
@@ -145,224 +118,3 @@ function WordRow(props) {
 
 export default WordRow;
 
-
-// import React from "react";
-// import "./style.css";
-// import { useEffect, useState, useCallback } from "react";
-
-// function WordRow(props) {
-// 	const { sent, score, sentenceCompleted, calcScore, validatingLetterIndex } =
-// 		props;
-
-// 	//validating id is used to pull the ids from the letters
-// 	console.log("validatingLetterIndex", validatingLetterIndex);
-
-// 	const sentArray = Object.values(sent);
-// 	const letterArray = sentArray.join(" ").split("");
-// 	const sentenceFinished = letterArray.length;
-
-// 	const [vi, setVi] = useState();
-
-// 	useEffect(() => {
-// 		window.addEventListener("keydown", handleKeyDown);
-// 		window.addEventListener("keyup", handleKeyUp);
-// 		setVi(validatingLetterIndex);
-// 		console.log(vi);
-
-// 		return () => {
-// 			window.removeEventListener("keydown", handleKeyDown);
-// 			window.removeEventListener("keyup", handleKeyUp);
-// 		};
-// 	}, []);
-// 	console.log(vi);
-
-	
-
-// 	// const incrementValIndex = (vi) => {
-// 	// 	vi++;
-// 	// 	console.log(vi);
-// 	// };
-
-// 	//counter used to increment for unique letter keys
-// 	var counter = 0;
-
-// 	//increments the counter for the key of each letter in the sentence
-// 	const counterFunc = () => {
-// 		counter++;
-// 		return counter;
-// 	};
-
-// 	//reset counter upon new sentence load
-// 	const resetCounter = () => {
-// 		return (counter = 0);
-// 	};
-
-// 	//keydown event checks the letter ids with the key pressed, using the validating id to ensure the letters are checked in order
-// 	const handleKeyDown = ({ key }, event) => {
-		
-// 		return validateKeycode(key, event);
-// 	};
-
-	
-// 	const validateKeycode = (key, event, vi) => {
-// 		let valueIndex = vi;
-// 		console.log('valueIndex', valueIndex);
-// 		console.log("key", key);
-		
-// 		let letterCol = document.getElementById(valueIndex);
-// 		let letterSpan = letterCol.firstElementChild;
-// 		console.log(letterSpan);
-// 		//if letter is space block, classlist is changed
-// 		if (letterSpan === null) {
-// 			if (key === " ") {
-// 				letterCol.classList.remove("spaceNotGuessed");
-// 				letterCol.classList.add("spaceGuessed");
-// 				valueIndex++;
-// 				setVi(valueIndex);
-				
-// 				console.log(vi);
-// 			} else {
-// 				alert("Please press the space key.");
-// 			}
-// 			//if letter space is not a space block, letter id is evaluated with the key pressed
-// 		} else {
-// 			var letterId = letterSpan.textContent.toLowerCase();
-// 			console.log(letterId);
-// 			console.log(vi);
-
-// 			if (letterId === key) {
-// 				letterCol.classList.remove("notGuessedSpace");
-// 				letterCol.classList.add("guessedSpace");
-// 				letterSpan.classList.remove("notGuessed");
-// 				letterSpan.classList.add("guessed");
-// 				// valIndex++;
-// 				valueIndex++;
-// 				setVi(valueIndex);
-// 				console.log(vi);
-
-// 				if (vi === sentenceFinished) {
-// 					console.log("sentenceFinished", sentenceFinished);
-// 					alert("Good job!");
-// 					resetCounter();
-// 					// resetVli();
-// 					props.sentenceCompleted();
-// 				}
-// 			}
-// 		}
-// 	};
-
-// 	// const resetVli = () => {
-// 	// 	return vli=1;
-// 	// }
-// 	// evalFunction();
-
-// 	const handleKeyUp = () => {};
-
-// 	// if (sentArray.length < 1) {
-// 	// 	return null;
-// 	// }
-
-// 	return (
-// 		<div>
-// 			{sentArray.map((word, index) => (
-// 				<div className="row" key={word + index}>
-// 					{word.split("").map((letter) => (
-// 						<div
-// 							id={counterFunc()}
-// 							key={letter.id}
-// 							className={`col letterSpace notGuessedSpace`}
-// 							onKeyDown={handleKeyDown}
-// 						>
-// 							<span className={"notGuessed"}>{letter}</span>
-// 						</div>
-// 					))}
-
-// 					{index < sentArray.length - 1 ? (
-// 						<div
-// 							id={counterFunc()}
-// 							onKeyDown={handleKeyDown}
-// 							key={"space" + index}
-// 							className={`col space spaceNotGuessed`}
-// 						>
-// 							{" "}
-// 						</div>
-// 					) : (
-// 						<span className={"notGuessed"}></span>
-// 					)}
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
-
-// export default WordRow;
-
-//for (let i = 0; i < sentenceFinished; i++) {
-// 		console.log('key pressed');
-// 		var letterCol = document.getElementById(i);
-// 		var letterSpan = letterCol.firstElementChild;
-// 		//if letter is space block, classlist is changed
-// 		if (letterSpan === null) {
-// 			if (key === " ") {
-// 				letterCol.classList.remove("spaceNotGuessed");
-// 				letterCol.classList.add("spaceGuessed");
-// 				// validatingId++;
-// 			} else {
-// 				alert("Please press the space key.");
-// 			}
-// 			//if letter space is not a space block, letter id is evaluated with the key pressed
-// 		} else {
-// 			var letterId = letterSpan.textContent.toLowerCase();
-
-// 			if (letterId === key) {
-// 				letterCol.classList.remove("notGuessedSpace");
-// 				letterCol.classList.add("guessedSpace");
-// 				letterSpan.classList.remove("notGuessed");
-// 				letterSpan.classList.add("guessed");
-
-// 				if (i === sentenceFinished) {
-// 					alert("Good job!");
-// 					resetCounter();
-// 					props.sentenceCompleted();
-// 				}
-// 			}
-// 		}
-// 	}
-// };
-
-//
-
-// console.log('validatingId', validatingId);
-// var letterCol = document.getElementById(validatingId);
-// var letterSpan = letterCol.firstElementChild;
-
-//if letter is space block, classlist is changed
-// 	if (letterSpan === null) {
-// 		if (key === " ") {
-// 			letterCol.classList.remove("spaceNotGuessed");
-// 			letterCol.classList.add("spaceGuessed");
-// 			validatingId++;
-// 		} else {
-// 			alert("Please press the space key.");
-// 		}
-// 		//if letter space is not a space block, letter id is evaluated with the key pressed
-// 	} else {
-// 		var letterId = letterSpan.textContent.toLowerCase();
-
-// 		if (letterId === key) {
-// 			letterCol.classList.remove("notGuessedSpace");
-// 			letterCol.classList.add("guessedSpace");
-// 			letterSpan.classList.remove("notGuessed");
-// 			letterSpan.classList.add("guessed");
-
-// 			if (validatingId === sentenceFinished) {
-// 				alert("Good job!");
-// 				resetCounter();
-
-// 				props.sentenceCompleted();
-// 			} else {
-// 				validatingId++;
-// 			}
-// 		}
-// 	}
-// };
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,38 +4,30 @@ import API from "../utils/API";
 import "../../src/App.css";
 import Container from "../components/Container";
 import WordRow from "../components/WordRow/";
-import { useSelector, useDispatch } from 'react-redux'
+
+window.apiCounter = 1; //NEW JN
+window.score = 0; //NEW JN
 
 const Homepage = () => {
 	//React hooks to render state
 	const [unscrambledSentence, setUnscrambledSentence] = useState("");
 	const [sentenceArray, setSentenceArray] = useState([]);
 	const [scrambledSentence, setScrambledSentence] = useState("");
-	// const [score, setScore] = useState(0);
-	// const [apiCounter, setApiCounter] = useState(1);
-	
-	
 
-	//initialization function
 	useEffect(() => {
-		console.log("homepage has mounted");
+		
 		loadGame();
 	}, []);
-
-	 window.apiCounter = 1;
-	 window.score = 0;
-	// var score = 0;
-	// var validatingLetterIndex = 1;
-
-	
 
 	const scrambleWord = (word) => {
 		var length = word.length;
 		var letterArr = word.split("");
 		var firstLetter = letterArr[0];
 		var lastLetter = letterArr[length - 1];
+
 		letterArr.pop();
 		letterArr.shift();
+
 		const scrambledInside = (letterArr) => {
 			for (var i = 0; i < length - 2; i++) {
 				var j = Math.floor(Math.random() * length);
@@ -47,15 +39,14 @@ const Homepage = () => {
 		};
 		return `${firstLetter}` + `${scrambledInside(letterArr)}` + `${lastLetter}`;
 	};
-
 	const scrambleSentence = (unscrambledSentence) => {
-		console.log(unscrambledSentence);
+		
 		const sentenceArr = unscrambledSentence.split(" ");
-		console.log(sentenceArr);
+
 		setSentenceArray(sentenceArr);
-		console.log(sentenceArray);
-		// sentenceArray.push(sentenceArr);
+
 		const newSentence = [];
+
 		sentenceArr.map((word) => {
 			if (word.length > 2) {
 				newSentence.push(scrambleWord(word));
@@ -64,61 +55,39 @@ const Homepage = () => {
 			}
 		});
 		setScrambledSentence(newSentence.join(" "));
-		// scrambledSentence = newSentence.join(" ");
 	};
 
-	//loads game; sets score; retrieves sentences from API
 	const loadGame = () => {
-		console.log("loadgame", window.apiCounter);
-		
 		API.getSentence(window.apiCounter)
 			.then((res) => {
 				const unscrambledSentence1 = res.data.data.sentence;
 				setUnscrambledSentence(unscrambledSentence1);
-				console.log(unscrambledSentence);
 				scrambleSentence(unscrambledSentence1);
 			})
 			.catch((err) => console.log(err));
 	};
 
-	//beginning of score calculation function
 	const calcScore = () => {
-		console.log('calcscore function', window.score);
-		window.score++;
-		// score++;
+		window.score++; 
 	};
 
 	const apiCount = () => {
 		console.log("apiCounter", window.apiCounter);
 		window.apiCounter++;
-		console.log('new apicounter', window.apiCounter);
 	};
-
-	// console.log("apiCounter", apiCounter);
-	// console.log("sentenceArray", sentenceArray);
-	// console.log("scrambledSentence", scrambledSentence);
-	// console.log("unScrambledSentence", unscrambledSentence);
-	// console.log("score", score);
 
 	const sentenceCompleted = () => {
 		console.log("in sentence finished function");
-		
 		calcScore();
 	};
 
 	const nextSentence = (event) => {
 		event.preventDefault();
 		console.log("button clicked");
-		// setValidatingLetterIndex(1);
-		// resetValidatingIndex();
 		apiCount();
 		loadGame();
 	};
-
-	// setValidatingLetterIndex(1);
-	console.log(sentenceArray);
-	// console.log('validatingLetterIndex', validatingLetterIndex);
-
+	
 	return (
 		<div>
 			<Container fluid>
@@ -127,13 +96,17 @@ const Homepage = () => {
 						<div className="row text-center" id="sentence-text">
 							<h1>{scrambledSentence}</h1>
 						</div>
+
 						<div className="row text-center" id="directions-text">
 							<p>Guess the sentence! Start typing</p>
+
 							<p>The yellow blocks are meant for spaces</p>
 						</div>
+
 						<h2 className="row text-center" id="score">
 							<p>Score: {window.score}</p>
 						</h2>
+
 						<div
 							className="row text-center justify-content-center"
 							id="directions-text"
@@ -144,9 +117,8 @@ const Homepage = () => {
 								key={window.apiCounter}
 								sentenceCompleted={sentenceCompleted}
 								calcScore={calcScore}
-								
-								// validatingLetterIndex={validatingLetterIndex}
 							/>
+
 							<div className="col-4">
 								<button
 									type="button"
@@ -166,3 +138,4 @@ const Homepage = () => {
 };
 
 export default Homepage;
+
